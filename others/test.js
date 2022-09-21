119c312b15c7d15d5adaf58ea41569da5ebe1fe6
rastaxarm <xarmzon@gmail.com>
2022-09-21 20:23:43 +0000

    update

---
+++ others/test.js
@@ -0,0 +1,10 @@
+let my_score = 97;
+let age;
+age = 56;
+const myName = "RastaXarm";
+const isPlaying = false;
+// isPlaying = true
+
+console.log(my_score);
+console.log(age);
+console.log(myName);
