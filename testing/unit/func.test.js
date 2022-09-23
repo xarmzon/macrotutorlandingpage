3365c03eb77a8a1d1ab0047d3789c64a37565a3c
rastaxarm <xarmzon@gmail.com>
2022-09-23 06:32:09 +0000

    update

--- testing/unit/func.test.js
+++ testing/unit/func.test.js
@@ -16,3 +16,19 @@ it("should add 3 + 5 = 8", () => {});
 //   it("should return new account data given a valid registration data",()=>{});
 //   it("should return error given invalid data", ()=>{})
 // });
+
+// describe("User Notes endpoints", ()=>{
+//   describe("GET /notes", ()=>{
+//     describe("Given a valid token", ()=>{
+//       it("should return all user notes", ()=>{
+//         expect(data).anyObject({title:expect.any(String)})
+//       })
+//     })
+//     describe("Given invalid token", ()=>{
+//       test("should return 400 error", ()=>{
+//         expect(statusCode).toBe(400)
+//         expect(body.msg).toMatch("unauthorized")
+//       })
+//     })
+//   })
+// })
